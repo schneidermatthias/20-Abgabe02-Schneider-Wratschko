4bc23376fe74cefda887c663e0471fb493eed83e
vinziw <vinzenz.wratschko@edu.campus02.at>
2020-07-06 00:04:35 +0200

    fixing JavaDoc in IQueue interface

--- src/main/java/at/fhj/iit/IQueue.java
+++ src/main/java/at/fhj/iit/IQueue.java
@@ -6,7 +6,7 @@ public interface IQueue<T> {
    * If the addition is successful, the method returns true else false.
    *
    * @param obj
-   * @return
+   * @return boolean
    */
   public abstract boolean offer(T obj);
 
@@ -14,7 +14,7 @@ public interface IQueue<T> {
    * Returns the head (first) element and also deletes it. That is, we cannot get it again.
    * If no element exists (when queue is empty), the method returns null.
    *
-   * @return
+   * @return T
    */
   public abstract T poll();
 
@@ -22,7 +22,7 @@ public interface IQueue<T> {
    * It also returns and deletes the head element like poll(), but with a small difference.
    * This method throws NoSuchElementException if the queue is empty.
    *
-   * @return
+   * @return T
    */
   public abstract T remove();
 
@@ -30,7 +30,7 @@ public interface IQueue<T> {
    * Returns the head element but it does not delete it. That is, we can get it again.
    * Returns null when the queue is empty.
    *
-   * @return
+   * @return T
    */
   public abstract T peek();
 
@@ -38,7 +38,7 @@ public interface IQueue<T> {
    * It works similar to peek() but with a small difference (returns but does not delete the element).
    * It throws NoSuchElementException when the queue is empty.
    *
-   * @return
+   * @return T
    */
   public abstract T element();
 }
